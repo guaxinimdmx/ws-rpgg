f6c65fbca095dbc1b13fcf84002f7f832d6a870d
André Ricardo <contatoadmx@gmail.com>
2021-10-29 00:16:13 -0300

    date

--- src/Chat.php
+++ src/Chat.php
@@ -1,43 +1,51 @@
 <?php
+
 namespace MyApp;
+
 use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
 
-class Chat implements MessageComponentInterface {
+class Chat implements MessageComponentInterface
+{
     protected $clients;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->clients = new \SplObjectStorage;
     }
 
-    public function onOpen(ConnectionInterface $conn) {
+    public function onOpen(ConnectionInterface $conn)
+    {
         // Store the new connection to send messages to later
         $this->clients->attach($conn);
 
         echo "New connection! ({$conn->resourceId})\n";
     }
 
-    public function onMessage(ConnectionInterface $from, $msg) {
+    public function onMessage(ConnectionInterface $from, $msg)
+    {
         $numRecv = count($this->clients) - 1;
 
         echo "Connection {$from->resourceId} sending message {$msg} to {$numRecv} connection";
 
         foreach ($this->clients as $client) {
-           // if ($from !== $client) {
-                // The sender is not the receiver, send to each client connected
-                $client->send($msg);
-           //}
+            // if ($from !== $client) {
+            // The sender is not the receiver, send to each client connected
+            $client->send(date('d/m/y H:i:s') . ": $msg");
+            //}
         }
     }
 
-    public function onClose(ConnectionInterface $conn) {
+    public function onClose(ConnectionInterface $conn)
+    {
         // The connection is closed, remove it, as we can no longer send it messages
         $this->clients->detach($conn);
 
         echo "Connection {$conn->resourceId} has disconnected\n";
     }
 
-    public function onError(ConnectionInterface $conn, \Exception $e) {
+    public function onError(ConnectionInterface $conn, \Exception $e)
+    {
         echo "An error has occurred: {$e->getMessage()}\n";
 
         $conn->close();
